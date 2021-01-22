8e2b008c00fa4d9d2e1cc9d43cbd669b9192acd1
nlsy <schlegel.nils@outlook.de>
2021-01-22 09:27:50 +0100

    Small Adjustments

--- VHDL/src/dbpul_a.vhd
+++ VHDL/src/dbpul_a.vhd
@@ -2,7 +2,7 @@
 
 ARCHITECTURE ar1 OF dbpul IS
 
-  constant all_ones : unsigned(dbc_width-1 DOWNTO 0) := (others => '1');
+  CONSTANT ones_s : unsigned(dbc_width-1 DOWNTO 0) := (others => '1');
 
   SIGNAL cnt_s : unsigned(dbc_width-1 DOWNTO 0); -- counter
   SIGNAL sig_s : std_logic;     -- stored signal
@@ -10,7 +10,7 @@ ARCHITECTURE ar1 OF dbpul IS
 
 BEGIN
 
-  PROCESS (rst_n_i,clk_i)
+  deb: PROCESS (rst_n_i,clk_i)
   BEGIN
     IF    (rst_n_i='0')               THEN cnt_s <= (others=>'0');
                                            sig_s <= unb_i;
@@ -21,7 +21,7 @@ BEGIN
       ELSE                    cnt_s <= cnt_s + 1;
       END IF;
 
-      IF (cnt_s = all_ones) THEN
+      IF (cnt_s = ones_s) THEN
         cnt_s <= (others=>'0');
         sig_s <= unb_i;
         IF (unb_i='1') THEN pul_s <= '1'; END IF;
@@ -29,7 +29,7 @@ BEGIN
       END IF;
 
     END IF;
-  END PROCESS;
+  END deb;
   deb_o <= pul_s;
 
 END ar1;
